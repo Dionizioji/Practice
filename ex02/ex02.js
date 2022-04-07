f59dcaff5917e287bbd723387f62c9125c7feb90
Dionízio <dionizioji@gmail.com>
2022-04-06 21:35:49 -0300

    Exercício02

--- ex02/ex02.js
+++ ex02/ex02.js
@@ -1,16 +1,18 @@
-const lado1 = 40
-const lado2 = 40
-const lado3 = 40
-
-function classifica (l1,l2,l3) {
-    if(l1!==l2 && l2!==l3){
-        return console.log("O triângulo é ESCALENO!")
+function clicked (event) {
+    event.preventDefault
+    let lado1 = parseInt(document.getElementsByClassName("triangle1"))
+    let lado2 = parseInt(document.getElementsByClassName("triangle2"))
+    let lado3 = parseInt(document.getElementsByClassName("triangle3"))
+    var result = document.getElementsByName("span")  
+    result.appendChild(document.createElement('div'))
+    /*if(l1!==l2 && l2!==l3){
+        return ("O triângulo é ESCALENO!")
     }
     else if(l1==l2 && l2==l3){
         return console.log("O triângulo é EQUILÁTERO!")
     }
     else{
         return console.log('O triângulo é ISÓSCELES!')
-    }
+    }*/
 }
-classifica(lado1,lado2,lado3)
+clicked()
