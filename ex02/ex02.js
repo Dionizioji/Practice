d95c7739fd4460884ceb153904ccd3fa845891b1
Dionízio <dionizioji@gmail.com>
2022-04-14 21:31:35 -0300

    Exercício 02

--- ex02/ex02.js
+++ ex02/ex02.js
@@ -1,18 +1,20 @@
-function clicked (event) {
-    event.preventDefault
-    let lado1 = parseInt(document.getElementsByClassName("triangle1"))
-    let lado2 = parseInt(document.getElementsByClassName("triangle2"))
-    let lado3 = parseInt(document.getElementsByClassName("triangle3"))
-    var result = document.getElementsByName("span")  
-    result.appendChild(document.createElement('div'))
-    /*if(l1!==l2 && l2!==l3){
-        return ("O triângulo é ESCALENO!")
+function clicking(){
+    var lado1 = document.getElementById("triangleOne")
+    var lado2 = document.getElementById("triangleTwo")
+    var lado3 = document.getElementById("triangleThree")
+    var result = document.getElementById("Resultado")  
+    if(eval(lado1.value)<=0 || eval(lado2.value)<=0 || eval(lado1.value)<=0){
+        window.alert("Valores inválidos!")
+    }else if(eval(lado1.value) !== eval(lado2.value)  && eval(lado2.value)  !== eval(lado3.value)){
+        result.innerText += " ESCALENO!"
+    }else if((eval(lado1.value) === eval(lado2.value)) && eval(lado2.value) === eval(lado3.value) && eval(lado1.value) === eval(lado3.value)){
+        result.innerText += " EQUILÁTERO!"
+    }else{
+        result.innerText += " ISÓSCELES!"
     }
-    else if(l1==l2 && l2==l3){
-        return console.log("O triângulo é EQUILÁTERO!")
-    }
-    else{
-        return console.log('O triângulo é ISÓSCELES!')
-    }*/
 }
-clicked()
+function clique(){
+    var result = document.getElementById("Resultado")
+    result.innerText = "O triângulo é: "
+    
+}
